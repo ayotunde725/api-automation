a1cb72b2af354dd3b4a2b2d2888e45ba4b3ae5fb
Dr.Yusuf Ayotunde <ayotunde725@gmail.com>
2026-07-27 14:11:05 -0700

    server.js

--- server.js
+++ server.js
@@ -10,5 +10,5 @@ server.use(middlewares);
 server.use(router);
 
 server.listen(port, () => {
-  console.log(JSON Server is running on port ${port});
+  console.log('JSON Server is running on port ${port}');
 });
